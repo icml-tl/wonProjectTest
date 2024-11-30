1550276baf5fa8ed9a7a4069f09658eff2b00ac2
icml-tl <ismailcraft5@gmail.com>
2024-11-30 14:08:28 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import dashboard from './views/dashboard.vue'
+</script>
+
+<template>
+  <dashboard></dashboard>
+  <RouterView />
+</template>
+
+<!-- <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header> -->
---
+++ src/views/dashboard.vue
@@ -0,0 +1,12 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup>
+import sideBar from '@/components/sideBar.vue'
+import dataContent from '@/components/dataContent.vue'
+</script>
+
+<template>
+  <div class="w-full min-h-screen">
+    <sideBar></sideBar>
+    <dataContent></dataContent>
+  </div>
+</template>
