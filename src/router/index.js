bbc28d7faeb92657f4717a627613f21db3145f77
icml-tl <ismailcraft5@gmail.com>
2024-11-30 16:52:11 +0100

    ui almost ready

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+// Using PascalCase for component names
 import dashboard from '@/views/dashboard.vue'
 
+const routes = [
+  {
+    path: '/',
+    name: 'dashboard',
+    component: dashboard,
+  },
+]
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'dashboard',
-      component: dashboard,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
+  history: createWebHistory(import.meta.env.BASE_URL), // base URL setup for Vite
+  routes,
 })
 
 export default router
